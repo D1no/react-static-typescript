377f83aed344b3ef688344032d0bd5ca5b9bcf40
Dino Scheidt <github@dino-scheidt.de>
2017-10-10 17:45:15 +0200

    ✨ Update the babel-resolution rule to enter via awesome-typescript-loader

--- static.config.js
+++ static.config.js
@@ -2,6 +2,9 @@ import axios from 'axios'
 import React, { Component } from 'react'
 import { ServerStyleSheet } from 'styled-components'
 
+// For TypeScript Resolution
+import { CheckerPlugin, TsConfigPathsPlugin } from "awesome-typescript-loader"
+
 export default {
   getRoutes: async () => {
     const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -27,7 +30,7 @@ export default {
     ]
   },
   Html: class CustomHtml extends Component {
-    render () {
+    render() {
       const { Html, Head, Body, children } = this.props
 
       const sheet = new ServerStyleSheet()
@@ -45,4 +48,43 @@ export default {
       )
     }
   },
+  webpack: (webpackConfigurator, { dev }) => {
+
+    // Add .ts & .tsx extension to config
+    webpackConfigurator.merge({
+      resolve: {
+        extensions: ['.ts', '.tsx']
+      }
+    })
+
+    // We can take a look at the current Webpack Config via webpackConfigurator._config
+    // Lets overwrite the default rule resolution for source files found here:
+    // https://github.com/nozzle/react-static/blob/master/src/rules.js#L5
+
+    // Update Rule to use awesome-typescript-loader
+    // Extending the default ruleset with typescript support (normal webpack rule)
+    let typeScriptRule = {
+      test: /\.(js|jsx|ts|tsx)$/,
+      // Keeping the original exclude path
+      exclude: webpackConfigurator._config.module.rules[0].exclude,
+      use: [
+        {
+          loader: 'babel-loader',
+        },
+        {
+          loader: 'awesome-typescript-loader',
+        }
+      ]
+    }
+
+    // Overwrite the first (original js-transform) rule with our extended version
+    webpackConfigurator._config.module.rules[0] = typeScriptRule;
+
+    // For async error reporting of as-typescript
+    webpackConfigurator.plugin("typescript-checker-plugin", CheckerPlugin)
+
+    // For resolving potential alias paths inside the typescript.json
+    webpackConfigurator.plugin("typescript-path-plugin", TsConfigPathsPlugin)
+
+  }
 }
